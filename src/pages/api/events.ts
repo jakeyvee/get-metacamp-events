d627c458b43a81b8fe5aa5bed54e801b3c0132e7
jakeyvee <jakeyveee@gmail.com>
2023-01-25 23:56:16 +0800

    init

---
+++ src/pages/api/events.ts
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    const data = await fetch("https://w3e.ai/backend/events/singapore")
+    const events = (await data.json()).result;
+    const metacampEvents = await events.filter(
+      (event: { organizer_name: string; }) => event.organizer_name.toLowerCase() === "metacamp"
+    );
+    res.status(200).json(metacampEvents);
+  } catch (err) {
+    res.status(404);
+  }
+}
