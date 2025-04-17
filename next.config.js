fcf8c7c82e9237190fb0f0a4e62946e4b1d56759
jonasXchen <jonas@metacamp.so>
2025-04-17 14:36:45 +0800

    update headers to metacamp.sg

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://www.metacamp.so" },
+          { key: "Access-Control-Allow-Origin", value: "https://www.metacamp.sg" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
